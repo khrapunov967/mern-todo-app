cddd4e7e37b2d7a09227fab42fb179d1cd3f4209
Andrew Khrapunov <andrew.khrapunov967@gmail.com>
2023-05-17 19:47:02 +0700

    add creds to cors

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,8 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173"
+    origin: "http://localhost:5173",
+    credentials: true
 }))
 // app.use((req, res, next) => {
 //     res
