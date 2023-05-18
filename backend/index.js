c90e46a026b5ba5a6d7c0acfda5680615fd8b1ae
Andrew Khrapunov <andrew.khrapunov967@gmail.com>
2023-05-18 12:02:59 +0700

    edit origin

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://mern-todo-app-client.onrender.com",
     credentials: true
 }))
 // app.use((req, res, next) => {
