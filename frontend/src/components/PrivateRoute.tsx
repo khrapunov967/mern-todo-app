6bce10e1c99aeb60d9bddd898253b0ba6c19b10c
Andrew Khrapunov <andrew.khrapunov967@gmail.com>
2023-05-24 10:33:33 +0700

    change title in private route component

--- frontend/src/components/PrivateRoute.tsx
+++ frontend/src/components/PrivateRoute.tsx
@@ -7,7 +7,7 @@ const PrivateRoute = () => {
     if (isAuth === undefined) return (
         <div className="z-10 w-full min-h-screen bg-[#000] bg-opacity-50 flex justify-center items-center transition-all duration-200">
             <div className="text-white text-3xl p-3 flex gap-4 bg-[#1c1c1c] rounded-lg">
-                Downloading...
+                Loading...
             </div>
         </div>
     );
